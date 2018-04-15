6f7186dfa89337d8e946677c59570e4895cf6063
Gabriel Porto Oliveira <gabrielpoliveira@ig.com.br>
2018-04-15 02:48:49 -0300

    0.0.7
    Semi 2 de LP agora com novo projeto netbeans

---
+++ Laboratório de Programação 2/projeto2 - grafos/cabecalho.h
@@ -0,0 +1,26 @@
+struct aresta {
+  int chave_adj; //destino
+  int peso;
+  struct aresta *prox;
+};
+typedef struct aresta Aresta;
+
+struct nodo {
+  int chave; //origem
+  struct nodo *prox;
+  struct aresta *adj;
+};
+typedef struct nodo Nodo;
+
+struct grafo {
+  struct nodo *listaNodos;
+  int tamanho;
+};
+typedef struct grafo Grafo;
+
+Nodo* criaNodo();
+Aresta* criaAresta();
+Grafo* criaGrafo();
+Aresta* insereAresta(Nodo* n, int chave, int peso);
+Grafo* lerArquivo();
+void imprimirGrafo(Grafo* g);
